2c299f372a375204ac517c577a2a013d065a2586
Stévan Corre <stevancorre@protonmail.com>
2022-05-20 23:46:32 +0200

    add silent option

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@
 import { loadConfigFile } from "./config";
 import { Configuration } from "./types";
 import { execute } from "./core";
+import { configureWriting } from "./utils/console";
 
 const config: Configuration = loadConfigFile("tests/simple/smart-copy.json");
+configureWriting(config);
 execute(config);
--- src/utils/console.ts
+++ src/utils/console.ts
@@ -1,3 +1,5 @@
+import { Configuration } from "../types";
+
 const RESET: string = <const>"\x1b[0m";
 
 const RED: string = <const>"\x1b[31m";
@@ -5,23 +7,30 @@ const GREEN: string = <const>"\x1b[32m";
 const YELLOW: string = <const>"\x1b[33m";
 const CYAN: string = <const>"\x1b[36m";
 
-export const writeWarning = (message: string): void => write("warning", YELLOW, message);
-export const writeError = (message: string): void => write("error", RED, message);
-export const writeNote = (message: string): void => write("note", CYAN, message);
-export const writeSuccess = (message: string): void => write("success", GREEN, message);
+let silent: boolean;
+
+export const configureWriting = (config: Configuration) => silent = config.options.silent;
+
+export const writeWarning = (message: string): void => write("warning", YELLOW, message, false);
+export const writeError = (message: string): void => write("error", RED, message, true);
+export const writeNote = (message: string): void => write("note", CYAN, message, false);
+export const writeSuccess = (message: string): void => write("success", GREEN, message, false);
 
 export const writeErrorAndNoteThenExit = (error: string, note: string): never => {
     writeError(error);
     writeNote(note);
-    
+
     return process.exit(1);
 };
 
 export const writeErrorThenExit = (error: string): never => {
     writeError(error);
-        
+
     return process.exit(1);
 };
 
-const write = (label: string, color: string, message: string): void =>
+const write = (label: string, color: string, message: string, force: boolean): void => {
+    if (silent && !force) return;
+
     console.log(`${color}${label}:${RESET} ${message}`);
+}    

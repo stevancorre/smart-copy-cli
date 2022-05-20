f6e9c39dd9f2286a9407ab0de96861b187e33ebc
Stévan Corre <stevancorre@protonmail.com>
2022-05-20 21:03:48 +0200

    [configuration] fix typo

--- src/types/Configuration.ts
+++ src/types/Configuration.ts
@@ -3,5 +3,5 @@ import { VarDictionary, ConfigurationOptions, Transaction } from ".";
 export type Configuration = {
     vars?: VarDictionary,
     options: ConfigurationOptions;
-    transations?: Transaction[] | Transaction;
+    transactions?: Transaction[] | Transaction;
 }

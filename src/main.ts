bdada6d97ff947281733dc08ab883cea055d4179
Stévan Corre <stevancorre@protonmail.com>
2022-05-22 01:09:27 +0200

    full working cli with commander

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+
 import { name, description, version } from "../package.json";
-import { bindCommands } from "./commands";
+import { DEFAULT_CONFIG_FILE_NAME, loadConfigFile } from "./config";
+import { Configuration, execute } from "./core";
+import { configureWriting } from "./utils/console";
 
 const program: Command = new Command();
 
 program
     .name(name)
     .description(description)
-    .version(version);
+    .version(version)
+    .argument("[config file]", "the configuration file", DEFAULT_CONFIG_FILE_NAME)
+    .action((configFile: string) => {
+        const configuration: Configuration = loadConfigFile(configFile);
+        configureWriting(configuration.options.silent);
 
-bindCommands(program);
+        execute(configuration);
+    })
+    .addCommand(program);
 
 program.parse();
